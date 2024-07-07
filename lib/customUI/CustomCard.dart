792a49ad7d30f9a9ba08c5a6842e221a0c4464a5
IsuraViranga <isuraviranga@gmail.com>
2024-07-07 21:43:33 +0530

    customCard updated

--- lib/customUI/CustomCard.dart
+++ lib/customUI/CustomCard.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomCard extends StatelessWidget {
   const CustomCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      leading:CircleAvatar(
-        radius: 30,
-      ),
-      title: Text(
-          "Amma",
-          style:TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold
-          ) ,
-      ),
-      subtitle: Row(
+    return InkWell(
+      onTap: (){},
+      child: Column(
         children: [
-          Icon(Icons.done_all),
-          SizedBox(
-            width: 3,
+          ListTile(
+            leading:CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.blueGrey,
+              child: SvgPicture.asset(
+                  "assets/person.svg",
+                  color: Colors.white,
+                  height:40,
+              ),
+            ),
+            title:const Text(
+                "Amma",
+                style:TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+                ) ,
+            ),
+            subtitle: const Row(
+              children: [
+                Icon(Icons.done_all),
+                SizedBox(
+                  width: 3,
+                ),
+                Text(
+                    "hello amma",
+                     style: TextStyle(
+                       fontSize: 13
+                     ),
+                )
+              ],
+            ),
+            trailing: const Text("18.4"),
           ),
-          Text("hello amm")
+          const Padding(
+            padding: EdgeInsets.only(left: 80,right: 20),
+            child:Divider(thickness: 1),
+          )
         ],
       ),
-      trailing: Text("18.4"),
     );
   }
 }
