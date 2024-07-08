27f04f4d95eaac75f903e46a3346526c141a669a
IsuraViranga <isuraviranga@gmail.com>
2024-07-08 15:06:21 +0530

    individual page text form designed as look like whatsappp

--- lib/Screens/individualPage.dart
+++ lib/Screens/individualPage.dart
@@ -94,6 +94,65 @@ class _IndividualPageState extends State<IndividualPage> {
                 }),
           ],
         ),
+        body:Container(
+          color: Colors.grey,
+          width:MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              ListView(),
+              Align(
+                alignment:Alignment.bottomCenter,
+                child: Row(
+                  children: [
+                    Container(
+                        width:MediaQuery.of(context).size.width -60,
+                        child: Card(
+                          margin: EdgeInsets.only(left: 4,right: 2,bottom: 8),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)
+                            ),
+                            child: TextFormField(
+                              textAlignVertical: TextAlignVertical.center,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 5,
+                              minLines: 1,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.all(5),
+                                hintText: "Type a message",
+                                prefixIcon: IconButton(
+                                  icon: Icon(Icons.emoji_emotions,color: Color(0xFF128C7E)),
+                                  onPressed: (){},
+                                ),
+                                suffixIcon:Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(onPressed: (){}, icon:Icon(Icons.attach_file,color: Color(0xFF128C7E),)),
+                                    IconButton(onPressed: (){}, icon:Icon(Icons.camera_alt,color: Color(0xFF128C7E))),
+                                  ],
+                                )
+                              ),
+                            )
+                        )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0,left: 2,right: 5),
+                      child: CircleAvatar(
+                        backgroundColor: Color(0xFF128C7E),
+                        radius: 25,
+                        child:IconButton(
+                          icon: Icon(Icons.mic,color: Colors.white,),
+                          onPressed: (){},
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
     );
   }
 }
