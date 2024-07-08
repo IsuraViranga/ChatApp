be0c95573fe77a439fb2f1aae8ed67addafd2f5e
IsuraViranga <isuraviranga@gmail.com>
2024-07-08 21:08:48 +0530

    showModalBottomSheet added for attachment button

--- lib/Screens/individualPage.dart
+++ lib/Screens/individualPage.dart
@@ -156,7 +156,16 @@ class _IndividualPageState extends State<IndividualPage> {
                                       suffixIcon:Row(
                                         mainAxisSize: MainAxisSize.min,
                                         children: [
-                                          IconButton(onPressed: (){}, icon:Icon(Icons.attach_file,color: Color(0xFF128C7E),)),
+                                          IconButton(
+                                              onPressed: (){
+                                                showModalBottomSheet(
+                                                    backgroundColor: Colors.transparent,
+                                                    context: context,
+                                                    builder:(builder)=>bottomSheet()
+                                                );
+                                              },
+                                              icon:Icon(Icons.attach_file,color: Color(0xFF128C7E))
+                                          ),
                                           IconButton(onPressed: (){}, icon:Icon(Icons.camera_alt,color: Color(0xFF128C7E))),
                                         ],
                                       )
@@ -207,5 +216,74 @@ class _IndividualPageState extends State<IndividualPage> {
         _textEditingController.text=_textEditingController.text + emoji.emoji;
       });
     },);
+  }
+
+  Widget bottomSheet() {
+    return Container(
+       height: 300,
+       width: MediaQuery.of(context).size.width,
+       child: Card(
+         shape: RoundedRectangleBorder(
+           borderRadius:BorderRadius.circular(25)
+         ),
+         margin: EdgeInsets.all(18),
+         child: Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+           child: Column(
+             children: [
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                    iconCreation(Icons.insert_drive_file, Colors.blueAccent, "Document"),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    iconCreation(Icons.camera_alt, Colors.red, "camera"),
+                     SizedBox(
+                       width: 25,
+                     ),
+                    iconCreation(Icons.insert_photo, Colors.purpleAccent, "Gallery"),
+                 ],
+               ),
+               SizedBox(
+                 height: 30,
+               ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   iconCreation(Icons.headset, Colors.orangeAccent, "Audio"),
+                   SizedBox(
+                     width: 25,
+                   ),
+                   iconCreation(Icons.location_pin, Colors.red, "Location"),
+                   SizedBox(
+                     width: 25,
+                   ),
+                   iconCreation(Icons.person, Colors.lightBlueAccent, "Contact"),
+                 ],
+               )
+             ],
+           ),
+         ),
+       ),
+    );
+  }
+  Widget iconCreation(IconData icon,Color color,String title){
+    return InkWell(
+      onTap: (){},
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor:color,
+            child: Icon(icon,color: Colors.white,),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(title)
+        ],
+      ),
+    );
   }
 }
